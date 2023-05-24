4952764fbaaafca10b0f28d5c108e32327bf6173
TinyikoNdlovu <tinyiko13@yahoo.com>
2023-05-24 17:21:05 +0200

    first update

--- password-generator.py
+++ password-generator.py
@@ -1,10 +1,10 @@
 import random
 
-password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+{}:?~"
+password_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#£$€%^&*()-+{}:?~[]<>/\;,-_=`|"
 length_password = int(input("Enter the length of the password: "))
-a = "".join(random.sample(password,length_password))
+a = "".join(random.sample(password_chars,length_password))
 
 if length_password >= 8:
-    print(f"Your password is {a}")
+    print(f"Your password is: {a}")
 else:
     print("Password Length is too short please retry")
